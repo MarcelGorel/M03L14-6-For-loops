7905acd431d28ee47197663165fddb7cd3d43c4e
MarcelGorel <66006966+MarcelGorel@users.noreply.github.com>
2021-03-14 21:20:54 +0100

    Add files via upload

---
+++ even-odd-reporter.js
@@ -0,0 +1,9 @@
+for (number = 1; number < 21; number++) {
+    if (number % 2 == 0) {
+        console.log(number, "is even");
+    }
+
+    else {
+        console.log(number, "is odd");
+    };
+}
---
+++ multiplication-tables.js
@@ -0,0 +1,19 @@
+// for (number = 1; number <= 10; number++) {
+
+//     console.log((number + " x " + 1 + " = " + (number * 1)));
+//     console.log((number + " x " + 2 + " = " + (number * 2)));
+//     console.log((number + " x " + 3 + " = " + (number * 3)));
+//     console.log((number + " x " + 4 + " = " + (number * 4)));
+//     console.log((number + " x " + 5 + " = " + (number * 5)));
+//     console.log((number + " x " + 6 + " = " + (number * 6)));
+//     console.log((number + " x " + 7 + " = " + (number * 7)));
+//     console.log((number + " x " + 8 + " = " + (number * 8)));
+//     console.log((number + " x " + 9 + " = " + (number * 9)));
+//     console.log((number + " x " + 10 + " = " + (number * 10)));
+// }
+
+
+for (i = 1; i <= 10; i++) {
+    for (j = 1; j <= 10; j++)
+        console.log(j + " X " + i + " = " + i * j);
+}
